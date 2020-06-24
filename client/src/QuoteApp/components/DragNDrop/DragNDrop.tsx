f2583973998cc628bbb761e3a00f4a6575ebda05
Shai Gilboa <shizel@gmail.com>
2020-06-24 13:28:17 -0400

    comments on DragNDrop

--- client/src/QuoteApp/components/DragNDrop/DragNDrop.tsx
+++ client/src/QuoteApp/components/DragNDrop/DragNDrop.tsx
@@ -1,21 +1,27 @@
 import React, { PropsWithChildren } from 'react';
 import styled from 'styled-components';
-import { MouseCoordinates } from './interfaces';
+import { Coordinates } from './interfaces';
 
 interface props {
   
 };
 
+// the movement is not dependent on knowing the location.
+// using transform: translate(Xpx, Ypx), means that the component
+// will move, but in RELATION to the starting point.
 const DragNDrop : React.FC<PropsWithChildren<props>> = ({children}) => {
-  // const componentRef = React.useRef<any>(null)
-  // const [currentValues, setCurrentValues] = React.useState<any>({left: 0, top: 0});
-  const [translateValues, setTranslateValues] = React.useState<any>({x:0, y:0})
-  const [mouseCoordinates, setMouseCoordinates] = React.useState<MouseCoordinates>({x:0, y:0})
-  const [translateUpToNow, setTranslateUpToNow] = React.useState<any>({x:0, y:0})
+  // how much the component will move on each drag
+  const [translateValues, setTranslateValues] = React.useState<Coordinates>({x:0, y:0})
+  // all the translations that have happened up to each iteration
+  // on mouse up we add up the translations of that current drag
+  const [translateUpToNow, setTranslateUpToNow] = React.useState<Coordinates>({x:0, y:0})
+  // where the mouse is
+  const [mouseCoordinates, setMouseCoordinates] = React.useState<Coordinates>({x:0, y:0})
+  // flag to know if the mouse has been clicked
   const [isMouseDown, setIsMouseDown] = React.useState<boolean>(false);
   
   // setting the position of everything for the start of the drag
-  const drag = React.useCallback(({clientX, clientY}) : void => {
+  const mouseDown = React.useCallback(({clientX, clientY}) : void => {
     setTranslateUpToNow({
       x: translateValues.x,
       y: translateValues.y
@@ -25,10 +31,14 @@ const DragNDrop : React.FC<PropsWithChildren<props>> = ({children}) => {
       y: clientY
     })
     setIsMouseDown(true)
-  }, [isMouseDown, , mouseCoordinates])
+  }, [isMouseDown, translateUpToNow])
 
+  // when the mouse moves the translations changes accordingly 
   const mouseMove = React.useCallback(({clientX, clientY}) : void =>{
-    if(isMouseDown && mouseCoordinates){
+    // only effective when the mouse is down
+    if(isMouseDown){
+      // each coordinate is set to be the value of the start of drag
+      // plus the sum of the movement of the mouse in relation to where it was clicked
       setTranslateValues({
         x: translateUpToNow.x + (clientX - mouseCoordinates.x),
         y: translateUpToNow.y + (clientY - mouseCoordinates.y)  
@@ -36,10 +46,12 @@ const DragNDrop : React.FC<PropsWithChildren<props>> = ({children}) => {
     }
   }, [isMouseDown, mouseCoordinates])
 
-  const mouseUp = React.useCallback(({clientX, clientY}): void => {
+  // when the mouse is up, set flag to false  
+  const mouseUp = React.useCallback((): void => {
     setIsMouseDown(false)
   }, [isMouseDown])
 
+  // on click we start a new listener
   React.useEffect(()=>{
     window.addEventListener('mousemove', mouseMove)
     window.addEventListener('mouseup', mouseUp)
@@ -48,15 +60,16 @@ const DragNDrop : React.FC<PropsWithChildren<props>> = ({children}) => {
       window.removeEventListener('mousemove', mouseMove)
       window.removeEventListener('mouseup', mouseUp)
     }
-  },[isMouseDown, mouseMove, mouseUp])
+  },[isMouseDown, mouseUp])
 
   return (
     <Wrapper
+    // the movement of the component is by translate
       style={{transform: `translate(${translateValues.x}px, ${translateValues.y}px)`}}
       onMouseDown={(event)=>{
         event.preventDefault();
         event.stopPropagation();
-        drag(event);
+        mouseDown(event);
       }}
     >
       {children}
--- client/src/QuoteApp/components/DragNDrop/interfaces.ts
+++ client/src/QuoteApp/components/DragNDrop/interfaces.ts
@@ -1,4 +1,4 @@
-export interface MouseCoordinates {
+export interface Coordinates {
   x : number,
   y : number
 }
