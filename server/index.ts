19d0bdfbb36d13c933913853bfa7b43e45ed28d7
Shai Gilboa <shizel@gmail.com>
2020-06-29 19:05:56 -0400

    server

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,28 @@
 import * as path from "path";
+import cors from "cors";
 import express from 'express';
 import setup from './routes/setup'
 const PORT = process.env.PORT || 4000;
 
 const app : express.Application = express()
 
+
+
 app.use(express.json());
+
+app.use(cors())
+  .use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Methods',
+      'OPTIONS, HEAD, GET, PUT, POST, DELETE'
+    );
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+  })
 
 app.use(setup);
 
--- server/routes/setup.ts
+++ server/routes/setup.ts
@@ -2,7 +2,9 @@ import * as express from 'express';
 
 const router = express.Router()
   .get('/setup/env', (req : express.Request, res : express.Response) : void => {
-    res.status(200).send({
+    console.log('variable-name')
+    res.status(200).json({
+      status: 200,
       test :'ff'
     })
   })
