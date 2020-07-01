2a46b8a5f434f2bc74cf9b0a6f3aaa6e23a66719
Craig Nockels <cnockels@gmail.com>
2020-07-01 11:05:38 -0400

    dotenv add

--- client/firebase/index.ts
+++ client/firebase/index.ts
@@ -1,11 +1,14 @@
 import { FirebaseConfigProcess } from "./types";
+import * as dotenv from 'dotenv';
 
-// export const firebaseConfig : FirebaseConfigProcess = {
-//   apiKey: process.env.REACT_APP_apiKey,
-//   authDomain: process.env.REACT_APP_authDomain,
-//   databaseURL: process.env.REACT_APP_databaseURL,
-//   projectId: process.env.REACT_APP_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_storageBucket,
-//   messagingSenderId: process.env.REACT_APP_messagingSenderId,
-//   appId: process.env.REACT_APP_appId
-// };
+dotenv.config();
+
+export const firebaseConfig : FirebaseConfigProcess = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  databaseURL: process.env.REACT_APP_databaseURL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
+};
